df3d6a2329f9ba4e67daa9b7d7425bbce2c51f81
phamvannam191199 <82355164+phamvannam191199@users.noreply.github.com>
2021-04-22 18:52:38 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* function main begins program execution */
+void main(void)
+{
+  printf("welcome to C!\n'");
+     printf("bau oi thuong lay bi cung\t ");
+     printf("tuy rang khac giong nhung chung mot gian\t");
+
+//return 0; /* indicate that program ended successfully */
+} /* end function main*/
